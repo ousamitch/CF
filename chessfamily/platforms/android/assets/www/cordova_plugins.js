ce9ceb5340f084205c61d99b7eba4c3793752fa8
oussema nachmi <nachmi.oussema@gmail.com>
2015-11-30 17:54:07 +0100

    v01

---
+++ chessfamily/platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,53 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "pluginId": "cordova-plugin-whitelist",
+        "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "id": "cordova-plugin-device.device",
+        "pluginId": "cordova-plugin-device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-network-information/www/network.js",
+        "id": "cordova-plugin-network-information.network",
+        "pluginId": "cordova-plugin-network-information",
+        "clobbers": [
+            "navigator.connection",
+            "navigator.network.connection"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-network-information/www/Connection.js",
+        "id": "cordova-plugin-network-information.Connection",
+        "pluginId": "cordova-plugin-network-information",
+        "clobbers": [
+            "Connection"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-battery-status/www/battery.js",
+        "id": "cordova-plugin-battery-status.battery",
+        "pluginId": "cordova-plugin-battery-status",
+        "clobbers": [
+            "navigator.battery"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.2.0",
+    "cordova-plugin-device": "1.1.0",
+    "cordova-plugin-network-information": "1.1.0",
+    "cordova-plugin-battery-status": "1.1.1",
+    "cordova-plugin-geolocation": "1.0.1"
+}
+// BOTTOM OF METADATA
+});
---
+++ chessfamily/www/js/main.js
@@ -0,0 +1,77 @@
+
+
+document.addEventListener("deviceready", onDeviceReady, false);
+ 
+function id(element) {
+    return document.getElementById(element);
+}
+
+function onDeviceReady() {
+	cameraApp = new cameraApp();
+    cameraApp.run();
+}
+
+function cameraApp(){}
+
+cameraApp.prototype={
+    _pictureSource: null,
+    
+    _destinationType: null,
+    
+    run: function(){
+        var that=this;
+	    that._pictureSource = navigator.camera.PictureSourceType;
+	    that._destinationType = navigator.camera.DestinationType;
+	    id("capturePhotoButton").addEventListener("click", function(){
+            that._capturePhoto.apply(that,arguments);
+        });
+    },
+    
+    _capturePhoto: function() {
+        var that = this;
+        
+        // Take picture using device camera and retrieve image as base64-encoded string.
+        navigator.camera.getPicture(function(){
+            that._onPhotoDataSuccess.apply(that,arguments);
+        },function(){
+            that._onFail.apply(that,arguments);
+        },{
+            quality: 50,
+            destinationType: that._destinationType.DATA_URL
+        });
+    },
+    
+    _getPhoto: function(source) {
+        var that = this;
+        // Retrieve image file location from specified source.
+        navigator.camera.getPicture(function(){
+            that._onPhotoURISuccess.apply(that,arguments);
+        }, function(){
+            cameraApp._onFail.apply(that,arguments);
+        }, {
+            quality: 50,
+            destinationType: cameraApp._destinationType.FILE_URI,
+            sourceType: source
+        });
+    },
+    
+    _onPhotoDataSuccess: function(imageData) {
+        var smallImage = document.getElementById('smallImage');
+        smallImage.style.display = 'block';
+    
+        // Show the captured photo.
+        smallImage.src = "data:image/jpeg;base64," + imageData;
+    },
+    
+    _onPhotoURISuccess: function(imageURI) {
+        var smallImage = document.getElementById('smallImage');
+        smallImage.style.display = 'block';
+         
+        // Show the captured photo.
+        smallImage.src = imageURI;
+    },
+    
+    _onFail: function(message) {
+        alert('Failed! Error: ' + message);
+    }
+}
